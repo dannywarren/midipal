5b26fddf07167b0597839a5cde30cbafc92b509e
Danny Warren <danny@dannywarren.com>
2016-02-25 22:59:35 -0800

    Adding script to make it easier to copy syx to desktop

--- make_sysex.sh
+++ make_sysex.sh
@@ -2,3 +2,8 @@
 
 # Convert midipal hex to proper sysex file
 python tools/hex2sysex/hex2sysex.py build/midipal/midipal.hex --page_size=64 --device_id=3 --syx --output_file build/midipal/midipal.syx
+
+VERSION=goliath
+DATE=`date +%Y%m%d00`
+
+cp build/midipal/midipal.syx $USERPROFILE/Desktop/midipal-$VERSION-$DATE.syx
